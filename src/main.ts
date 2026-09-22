0cb70dd400a619a95fea2d7a43a984063e5f9fd5
OmR31997 <omr31997@gmail.com>
2026-09-22 18:09:09 +0530

    add: cors origin .surf

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
+      'https://indianpublicschool.devs.surf',
       'https://indian-public-school-app.vercel.app',
       'https://omr31997.github.io/indian-public-school-app'
     ],
